a931b6ac216e16529c923c2f931adcff9af36d88
Bryant Lee <bl692@cornell.edu>
2022-03-27 20:20:06 -0400

    Add X,Y macro for retry text

--- src/game_over.c
+++ src/game_over.c
@@ -21,16 +21,16 @@ void enter_game_over_state(uint8_t *game_state) {
     *game_state = GAME_OVER_STATE;
     HIDE_SPRITES;
     set_bkg_tiles(GAME_OVER_X, GAME_OVER_Y, GAME_OVER_TEXT_TILEMAP_WIDTH, GAME_OVER_TEXT_TILEMAP_HEIGHT, game_over_text_tilemap);
-    set_bkg_tiles(0, 0, RETRY_TEXT_TILEMAP_WIDTH, RETRY_TEXT_TILEMAP_HEIGHT, retry_text_tilemap);
+    set_bkg_tiles(RETRY_TEXT_X, RETRY_TEXT_Y, RETRY_TEXT_TILEMAP_WIDTH, RETRY_TEXT_TILEMAP_HEIGHT, retry_text_tilemap);
 }
 
 void update_game_over(uint16_t *game_over_animation_timer) {
     if (*game_over_animation_timer == 0) {
-		set_bkg_tiles(0, 0, RETRY_TEXT_TILEMAP_WIDTH, RETRY_TEXT_TILEMAP_HEIGHT, retry_text_tilemap); 
+		set_bkg_tiles(RETRY_TEXT_X, RETRY_TEXT_Y, RETRY_TEXT_TILEMAP_WIDTH, RETRY_TEXT_TILEMAP_HEIGHT, retry_text_tilemap); 
 		*game_over_animation_timer = ANIMATION_CYCLE_TIME;
 	}
 	else if(*game_over_animation_timer == HALF_ANIMATION_CYCLE_TIME) {
-		set_bkg_tiles(0, 0, RETRY_TEXT_TILEMAP_WIDTH, RETRY_TEXT_TILEMAP_HEIGHT, nature_background_tilemap); 
+		set_bkg_tiles(RETRY_TEXT_X, RETRY_TEXT_Y, RETRY_TEXT_TILEMAP_WIDTH, RETRY_TEXT_TILEMAP_HEIGHT, nature_background_tilemap); 
 		(*game_over_animation_timer) --;
 	}
 	else {
