050f45975bf985911b19a425043594a7a556a331
Bryant Lee <bl692@cornell.edu>
2024-02-12 21:38:37 -0500

    Update SGB border graphics

--- res/sgb_border_tiles.h
+++ res/sgb_border_tiles.h
@@ -1,5 +1,5 @@
-#ifndef _SGB_BORDER_TILES_H
-#define _SGB_BORDER_TILES_H
+#ifndef METASPRITE_border_data_H
+#define METASPRITE_border_data_H
 
 #include <stdint.h>
 #include <gbdk/platform.h>
@@ -10,10 +10,10 @@
 #define border_data_TILE_H 8
 #define border_data_WIDTH 256
 #define border_data_HEIGHT 224
-#define border_data_TILE_COUNT 151
-#define border_data_PALETTE_COUNT 1
+#define border_data_TILE_COUNT 235
+#define border_data_PALETTE_COUNT 2
 #define border_data_COLORS_PER_PALETTE 16
-#define border_data_TOTAL_COLORS 16
+#define border_data_TOTAL_COLORS 32
 #define border_data_MAP_ATTRIBUTES 0
 #define border_data_MAP_ATTRIBUTES_WIDTH 32
 #define border_data_MAP_ATTRIBUTES_HEIGHT 28
@@ -22,8 +22,9 @@
 
 BANKREF_EXTERN(border_data)
 
-extern const palette_color_t border_data_palettes[16];
-extern const uint8_t border_data_tiles[4832];
+extern const palette_color_t border_data_palettes[32];
+extern const uint8_t border_data_tiles[7520];
+
 extern const unsigned char border_data_map[1792];
 
 #endif
